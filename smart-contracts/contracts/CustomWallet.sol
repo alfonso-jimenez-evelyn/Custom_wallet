c14307fccb87ab9c5202d250a2595485a914105f
Alfonso Jimenez <ajimenez@agros.tech>
2022-08-13 19:16:14 -0500

    feat: contract flow updated

---
+++ smart-contracts/contracts/CustomWallet.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CustomWallet {
+    uint public unlockTime;
+    address payable public owner;
+
+    event Withdrawal(uint amount, uint when);
+
+    constructor(uint _unlockTime) payable {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+
+        unlockTime = _unlockTime;
+        owner = payable(msg.sender);
+    }
+
+    function createIdentity()
+    
+    function verifyIdentity()
+
+    function revokeIdentity()
+}
